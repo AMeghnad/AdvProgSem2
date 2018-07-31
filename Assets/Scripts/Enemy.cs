9a5e568630f6dd1b57a53cdb3934da7497dc8a25
AMeghnad <anshumanmeghnad@gmail.com>
2018-07-31 12:05:17 +1000

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private int health = 100;
+
+    public int Health
+    {
+        get
+        {
+            return health;
+        }
+    }
+    public NavMeshAgent agent;
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    public void DealDamage(int damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public BoxCollider hitbox;
+    public int damage = 50;
+    // attribute
+    [Tooltip("Duration hitbox is enabled(in seconds)")]
+    public float hitDuration = 1f; // duration hitbox is enabled
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if space is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Run hit sequence
+            StartCoroutine(Hit());
+        }
+    }
+
+    IEnumerator Hit()
+    {
+        hitbox.enabled = true;
+        yield return new WaitForSeconds(hitDuration);
+        hitbox.enabled = false;
+    }
+
+    // OnTriggerEnter is called when the Collider other enters the trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        // detect enemy
+        Enemy enemy = GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            // deal damage
+            enemy.DealDamage(damage);
+        }
+    }
+}
